a56392361c09479dd5b2b811483bf0777ca4dca7
A-dot-hub <amanjaiswar295@gmail.com>
2026-05-27 11:16:36 +0530

    feat: configure socket connection with reconnection attempts and timeout settings

--- client/js/socket.js
+++ client/js/socket.js
@@ -1,6 +1,10 @@
-const socket = io();
+
+const socket = io({
+  reconnectionAttempts: 5,
+  timeout: 10000
+});
 window.gameSocket = {
   socket,
   join: (name) => socket.emit("join-game", name),
-  guess: (val) => socket.emit("submit-guess", val),
+  guess: (val) => socket.emit("submit-guess", val)
 };
